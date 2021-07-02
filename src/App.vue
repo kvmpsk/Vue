064f902585ddc184e38aac0789671c9598c55f33
VKosterin <kvm4000@gmail.com>
2021-07-02 17:12:27 +0300

    create lesson1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" :date="currentDate"/>
+    <button v-on:click="onclick">Click me </button> / {{ count }}
+    <button @:click="count = 1 + 2">Sum</button>
+    <button @:click="onclick2(1, 4)">Click me 2</button>
+
   </div>
 </template>
 
@@ -12,6 +16,23 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      count: 0,
+      currentDate: new Date().toDateString(),
+    }
+  },
+  methods: {
+    onclick() {
+      this.count +=1
+    },
+    onclick2(param1, param2) {
+      this.count = param1 + param2
+    }
+  },
+  computed: {
+
   }
 }
 </script>
