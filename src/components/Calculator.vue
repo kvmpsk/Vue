8cdb22daf1749589c1cd3e37d0ad7c23ffa721b8
VKosterin <kvm4000@gmail.com>
2021-07-09 16:33:38 +0300

    DZ lesson2

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -19,24 +19,26 @@
       </button>
     </div>
 
+    <input type="checkbox" id="display" @click="keybuttonVisible = !keybuttonVisible">
+    <label for="display" >Отобразить экранную клавиатуру</label>
 
-    <div class="keybutton">
-      <button v-for="(item, idx) in calcButton" :key="idx">
-        {{ item }}
-      </button>
+    <div class="keybutton" v-if="keybuttonVisible">
+      <div>
+        <button v-for="(item, idx) in calcButton" :key="idx">
+          {{ item }}
+        </button>
+      </div>
+
+      <div class="check_operand">
+        <label for="operand1">
+          <input type="radio" id="operand1"><span>Операнд1</span>
+        </label>
+
+        <label for="operand2">
+          <input type="radio" id="operand2"><span>Операнд2</span>
+        </label>
+      </div>
     </div>
-
-    <div class="check_operand">
-      <label for="operand1">
-        <input type="radio" id="operand1"><span>Операнд1</span>
-      </label>
-
-      <label for="operand2">
-        <input type="radio" id="operand2"><span>Операнд2</span>
-      </label>
-
-    </div>
-
   </div>
 
 
@@ -47,6 +49,7 @@ export default {
   name: "Calculator",
   data() {
     return {
+      keybuttonVisible: true,
       operand1: 0,
       operand2: 0,
       result: 0,
@@ -104,9 +107,10 @@ export default {
 }
 .keybutton {
   margin-top: 20px;
+  margin-bottom: 10px;
 }
 .keyboard {
   margin-top: 20px;
-  margin-bottom: 5px;
+  margin-bottom: 10px;
 }
 </style>
