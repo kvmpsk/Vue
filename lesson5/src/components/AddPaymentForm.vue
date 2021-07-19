f6b49e0274f4e19f3f236d15a5f45036e18af450
VKosterin <kvm4000@gmail.com>
2021-07-19 11:53:21 +0300

    lesson5

---
+++ lesson5/src/components/AddPaymentForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="payment-form">
+    <input placeholder="Amout" type="number" v-model.number="value" />
+    <!-- <input placeholder="Category" type="text" v-model.trim="category" /> -->
+    <select v-model="category">
+      <option v-for="(item, idx) in categoryList" :value="item" :key="idx">
+        {{item}}
+      </option>
+    </select>
+    <input placeholder="Date" type="text" v-model.trim="date" />
+    <button @click="onClick"> Save </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddPaymentForm",
+  props:['categoryList'],
+  data(){
+    return {
+      value: 0,
+      category: 'Food',
+      date: ''
+    }
+  },
+  methods:{
+    getCurrentDate(){
+      const today= new Date()
+      const d = today.getDate()
+      const m = today.getMonth() + 1
+      const y = today.getFullYear()
+      return `${d}.${m}.${y}`
+    },
+    onClick(){
+      const {value, category} = this
+      const data = {
+        date: this.date || this.getCurrentDate(),
+        category,
+        value
+      }
+      console.log(data)
+      this.$emit('addNewPayment', data)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
