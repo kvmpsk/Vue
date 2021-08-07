1405b7495ef24140255466c473fc29a5862f3fb9
VKosterin <kvm4000@gmail.com>
2021-08-07 12:08:36 +0300

    lesson1

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,23 +4,12 @@
     <input type="number" v-model.number="operand2" placeholder="operand2"/>
     = {{ result }}
 
-    <div class="error"
-         :class="{'red': error}">
-         {{ error }}
-    </div>
-
       <div class="keyboard">
         <button @click="calculate('+')">+</button>
         <button @click="calculate('-')">-</button>
         <button @click="calculate('*')">*</button>
         <button @click="calculate('/')">/</button>
-        <button @click="calculate('Целочисленное деление')">Целочисленное деление</button>
-
-
       </div>
-
-      <button @click="powWithOperand">Возведение в степень</button>
-       = {{ powWithOperand }}
 
   </div>
 </template>
@@ -28,65 +17,29 @@
 <script>
 export default {
   name: "Calculator",
-  data() {
-    return {
-      operand1: 0,
-      operand2: 0,
-      result: 0,
-      error: ""
-    }
-  },
+  data: () => ({
+    operand1: 0,
+    operand2: 0,
+    result: 0,
+  }),
+
   methods: {
-    calculate(operation = "+") {
-      this.error = "";
-      switch (operation) {
-        case '+':
-          this.add()
-          break;
-        case '-':
-          this.substract()
-          break;
-        case '*':
-          this.multiply()
-          break;
-        case '/':
-          this.divide()
-          break;
-        case 'Целочисленное деление':
-          this.whole()
-          break;
+    calculate(operation) {
+      const {operand1, operand2} = this
+      const calcOperation = {
+        '+': () => operand1 + operand2,
+        '-': () => operand1 - operand2,
+        '*': () => operand1 * operand2,
+        '/': () => operand1 / operand2,
       }
+      this.result = calcOperation[operation]()
     },
-    add() {
-      this.result = this.operand1 + this.operand2
-    },
-    substract() {
-      this.result = this.operand1 - this.operand2
-    },
-    multiply() {
-      this.result = this.operand1 * this.operand2
-    },
-    divide() {
-      const { operand1, operand2 } = this
-      if (operand2 === 0) {
-        return this.error = "Делить на 0 нельзя"
-      }
-      this.result = operand1 / operand2
-    },
-    whole() {
-      this.result = Math.floor(this.operand1 / this.operand2)
-    }
-  },
-  computed: {
-    powWithOperand() {
-      return Math.pow(this.operand1, this.operand2)
-    }
   }
 }
+
+
 </script>
 
 <style scoped>
-  .red {
-    color: red;
-  }
+
 </style>
